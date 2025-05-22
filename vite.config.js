9e7cfe03fc56e5f8acc157f40c174a07a6e8781f
FFMahani <fatemeh.fouladi.eng@gmail.com>
2025-05-22 13:27:56 -0400

    Update vite.config.js for correct base path with deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  base: '/techstore-frontend/',
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/techstore-frontend/' : '/',
   plugins: [react()],
-});
+}));
