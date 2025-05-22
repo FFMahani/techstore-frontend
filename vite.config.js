11049e9980c367e1c72277a9855fad4999a0cfd4
FFMahani <fatemeh.fouladi.eng@gmail.com>
2025-05-22 12:00:37 -0400

    Remove homepage from package.json and update vite config for HashRouter deployment

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  return {
-    base: mode === 'production' ? '/techstore-frontend/' : '/',
-    plugins: [react()],
-  }
+export default defineConfig({
+  base: '/',
+  plugins: [react()],
 });
