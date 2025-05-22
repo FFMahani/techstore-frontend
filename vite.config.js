4e42f70e77f6c2dfe409000bbfa008c511557072
FFMahani <fatemeh.fouladi.eng@gmail.com>
2025-05-22 11:46:23 -0400

    Update vite config and remove .env files from Git tracking

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: import.meta.env.VITE_APP_BASE_URL || '/',  // Use import.meta.env for Vite environment variables
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === 'production' ? '/techstore-frontend/' : '/',
+    plugins: [react()],
+  }
 });
