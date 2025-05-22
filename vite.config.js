2ddff829d28ca2695012273e524df2ae864f76f9
FFMahani <fatemeh.fouladi.eng@gmail.com>
2025-05-22 11:06:23 -0400

    Add .env and .env.production to .gitignore

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: import.meta.env.MODE === 'production' ? '/techstore-frontend/' : '/',
+  base: import.meta.env.VITE_APP_BASE_URL || '/',  // Use import.meta.env for Vite environment variables
   plugins: [react()],
-})
+});
